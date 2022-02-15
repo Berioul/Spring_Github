84aaf17009f3a4529d7df7f69f2b46093bd01ea0
Oualid Sahbani <oualidsahbani@me.com>
2022-02-15 11:17:02 +0100

    first

---
+++ src/main/java/de/neuefische/secondspringinit/SecondSpringINitApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.secondspringinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringINitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpringINitApplication.class, args);
+    }
+
+}
