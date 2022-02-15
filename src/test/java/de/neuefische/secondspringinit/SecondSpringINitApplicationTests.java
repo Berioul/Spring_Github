9e2a50ae8e108f7a8abdadd0bab622607916cdf5
Oualid Sahbani <oualidsahbani@me.com>
2022-02-15 11:52:46 +0100

    fail Test

--- src/test/java/de/neuefische/secondspringinit/SecondSpringINitApplicationTests.java
+++ src/test/java/de/neuefische/secondspringinit/SecondSpringINitApplicationTests.java
@@ -1,5 +1,7 @@
 package de.neuefische.secondspringinit;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SecondSpringINitApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail();
     }
 
 }
